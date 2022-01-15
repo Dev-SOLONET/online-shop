db277da4745e09a1b82141060659046f8f12225d
edy-kurniawan <edy.kurniawan280898@gmail.com>
2022-01-15 21:56:57 +0700

    bug icon & layout

--- resources/views/layouts/argon.blade.php
+++ resources/views/layouts/argon.blade.php
@@ -46,8 +46,22 @@
                         </button>
                     </form>
                     <!-- Navbar links -->
-                    <ul class="navbar-nav align-items-center  ml-md-auto ">
-                        
+                    <ul class="navbar-nav align-items-center  ml-md-auto">
+                        <li class="nav-item d-xl-none">
+                            <!-- Sidenav toggler -->
+                            <div class="pr-3 sidenav-toggler sidenav-toggler-dark" data-action="sidenav-pin" data-target="#sidenav-main">
+                              <div class="sidenav-toggler-inner">
+                                <i class="sidenav-toggler-line"></i>
+                                <i class="sidenav-toggler-line"></i>
+                                <i class="sidenav-toggler-line"></i>
+                              </div>
+                            </div>
+                          </li>
+                        <li class="nav-item d-sm-none">
+                            <a class="nav-link" href="#" data-action="search-show" data-target="#navbar-search-main">
+                              <i class="ni ni-zoom-split-in"></i>
+                            </a>
+                        </li>
                     </ul>
                     <ul class="navbar-nav align-items-center  ml-auto ml-md-0 ">
                         <li class="nav-item dropdown">
